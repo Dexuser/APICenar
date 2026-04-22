cfd4c4750d7b329a8a04bbcb5a858f96fe067e6e
Luis Angel Ureña Perdomo <urenaperdomo@gmail.com>
2026-04-21 23:41:51 -0400

    CORS agregado

--- app.js
+++ app.js
@@ -7,12 +7,29 @@ import apiRouter from "./routes/api-router.js";
 import { projectRoot } from "./utils/Paths.js";
 import { sendError, sendSuccess } from "./utils/apiResponses.js";
 import connectDB from "./utils/MongooseConnection.js";
+import cors from "cors";
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(projectRoot, "public")));
+
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN
+      ? process.env.CORS_ORIGIN.split(",")
+      : "*",
+
+    methods: process.env.CORS_METHODS
+      ? process.env.CORS_METHODS.split(",")
+      : ["OPTIONS", "GET", "POST", "PUT", "PATCH", "DELETE"],
+
+    allowedHeaders: process.env.CORS_ALLOWED_HEADERS
+      ? process.env.CORS_ALLOWED_HEADERS.split(",")
+      : ["Content-Type", "Authorization"],
+  })
+);
 
 app.get("/", (req, res) =>
   sendSuccess(res, 200, {
